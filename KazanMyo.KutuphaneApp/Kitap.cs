1f2f0694357c9197f36a6c43e4a9eb01fd4a600d
Elif Arzu Aksu <blackpsychocat@gmail.com>
2020-05-22 23:29:41 +0300

    Add project files.

---
+++ KazanMyo.KutuphaneApp/Kitap.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KazanMyo.KutuphaneApp
+{
+    class Kitap
+    {
+
+        public Kitap()
+        {
+
+        }
+
+        public string kitapyazar
+        {
+            get { return kitapyazar; } set { kitapyazar = value; }
+        }
+
+
+        public string kitaptur
+        {
+            get { return kitaptur; } set { kitaptur = value; }
+        }
+
+        public string kitapad
+        {
+            get { return kitapad; } set { kitapad = value; }
+        }
+
+        public DateTime kitapbastarih { get;  set; }
+
+        public Kitap(string kitapad, string kitapyazar, string kitaptur, DateTime kitapbastarih)
+        {
+            kitapad = kitapad;
+            kitapyazar = kitapyazar;
+            kitaptur = kitaptur;
+            kitapbastarih = kitapbastarih;
+        }
+        public static void dosyaekle(Kitap[] kitaplar)
+        {
+            try
+            {
+                string dyolu = @"D:\metinbelgesi.txt";
+                bool varmi = File.Exists(dyolu);
+                FileStream fs = new FileStream(dyolu, FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                if (varmi == false) sw.WriteLine("*****Kitap Adı******     *****Kitap Yazarı********     ******Kitap Türü******   ****Kitap Basım Yılı*****");
+                foreach (var kitap in kitaplar)
+                {
+                    sw.Write("\r\nKitap adı : " + kitap.kitapad);
+                    sw.Write("\r\nKitabın yazarı : " + kitap.kitapyazar);
+                    sw.Write("\r\nKitabın türü : " + kitap.kitaptur);
+                    sw.Write("\r\nKitabın basım yılı : " + kitap.kitapbastarih.ToShortDateString() + "\n\n");
+                }
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Dosya ekleme sırasında hata oluştu. " + e.Message);
+            }
+        }
+    }
+}
---
+++ KazanMyo.KutuphaneApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KazanMyo.KutuphaneApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçin");
+                    Console.WriteLine("***Kitap Ekleme için 1 ***\n***Kitabevindeki kitapların listesini görüntülemek için 2 ***\n***Uygulamadan çıkmak için 3 ***\nKitap basım tarihini düzgün şekilde girin.\n");
+
+                    Console.Write("Yapmak istediğiniz işlemin numarasını yazın = ");
+
+                    int is_no = int.Parse(Console.ReadLine());
+
+                    if (is_no == 1)
+                    {
+                        Console.Write("İstediğiniz kitap sayısını yazın = ");
+                        int kitapsayisi = int.Parse(Console.ReadLine());
+                        Kitap[] Kitaplar = new Kitap[kitapsayisi];
+                        int i = 0;
+                        do
+                        {
+                            Kitap kitap = new Kitap();
+                            Console.Write("Kitap adını girin = ");
+                            kitap.kitapad = Console.ReadLine();
+                            Console.Write("Kitap yazarı adını girin = ");
+                            kitap.kitapyazar = Console.ReadLine();
+                            Console.Write("Kitap basım tarihini girin = ");
+                            kitap.kitapbastarih = DateTime.Parse(Console.ReadLine());
+                            if (2020 < kitap.kitapbastarih.Year)
+                            {
+                                Console.WriteLine("Girilen sayı 2020 den büyük olamaz. Sayıyı tekrar girin.");
+                                continue;
+                            }
+                            Console.Write("Kitap türünü girin = ");
+                            kitap.kitaptur = Console.ReadLine();
+
+                            Kitaplar[i] = kitap;
+                            i++;
+                        } while (i < kitapsayisi);
+                        Kitap.dosyaekle(Kitaplar);
+                        Console.WriteLine("Kitaplarınız eklendi");
+                    }
+                    else if (is_no == 2)
+                    {
+                        string dyolu = @"D:\metinbelgesi.txt";
+                        string line;
+                        StreamReader file = new StreamReader(dyolu);
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            Console.WriteLine(line);
+                        }
+                        file.Close();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Uygulamadan çıkış yaptın.");
+                        Environment.Exit(0);
+                        break;
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Bir sayı girin \t!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bir hata oluştu !");
+            }
+            Console.ReadLine();
+        }
+    }
+}
